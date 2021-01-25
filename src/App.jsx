699d0c7d520df419bb9e10ebefd09818bb52ef13
Adam Morsi <adam.morsi@yahoo.com>
2021-01-25 14:05:14 +0200

    add disconnect functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React, { lazy, Suspense, useEffect } from 'react';
 import { hot } from 'react-hot-loader/root';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 //actions
-import { startSocketConnection } from './js/store/app/actions/AppActions';
+import { startSocketConnection, stopSocketConnection } from './js/store/app/actions/AppActions';
+//selectors
+import { getConnectionStatus } from './js/store/app/selectors/AppSelectors';
 //components
 import LoadingIndicator from './js/components/UI/LoadingIndicator';
 import ChatBox from './js/containers/ChatBox';
 const TestComponent = lazy(() => import('./js/components/TestComponent'));
 
 const App = () => {
-	const dispatch = useDispatch();
+	const dispatch = useDispatch(),
+		connectionStatus = useSelector((state) => getConnectionStatus({ state }));
 
 	useEffect(() => {
 		dispatch(startSocketConnection());
+
+		return () => {
+			if (connectionStatus === 'connected') {
+				dispatch(stopSocketConnection());
+			}
+		};
 	}, []);
 
 	return (
